dbadb0a47498e347080778102f8f8bcef482a778
Wojciech Walaszek <wwalaszek02@gmail.com>
2021-08-25 01:09:39 +0200

    bsdiff packaging

---
+++ packaging/launcherBindiffs.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+
+import sys
+import getopt
+import urllib.request
+from github import Github
+
+system = ''
+tag = ''
+outputdir = ''
+winplatform = ''
+repository = ''
+
+
+def do_bsdiff(url, newfile):
+    # gets filename from url
+    idx = asset.browser_download_url.rfind('/')
+    oldfile = outputdir + asset.browser_download_url[idx:end]
+
+    urllib.request.urlretrieve(asset.browser_download_url, oldfile)
+    update = "update-" + newfile
+    downgrade = "downgrade" + newfile
+
+    os.system("bsdiff " + oldfile + " " + newfile + " " + update)
+    os.system("bsdiff " + newfile + " " + oldfile + " " + downgrade)
+
+    os.remove(oldfile)
+    pass
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "s:t:o:r:", [
+                               "system=", "tag=", "outputdir=", "repository="])
+except getopt.GetoptError:
+    print("launcherBindiffs.py -s <system> -t <tag> -o <outputdir> -r <repository>")
+    sys.exit(2)
+
+for opt, arg in opts:
+    print(opt, " - ", arg)
+    if opt in ("-s", "--system"):
+        system = arg
+    elif opt in ("-t", "--tag"):
+        tag = arg
+    elif opt in ("-o", "--outputdir"):
+        outputdir = arg
+    elif opt in ("-r", "--repository"):
+        repository = arg
+
+print("Getting latest release")
+g = Github()
+repo = g.get_repo(repository)
+assets = repo.get_latest_release().get_assets()
+
+os.path.isfile(path)
+
+linuxnewfile = os.environ[OUTPUTDIR] + "/" + \
+    os.environ[PACKAGING_INSTALLER_NAME] + "-" + tag + "-x86_64.AppImage"
+macoscompatnewfile = os.environ[OUTPUTDIR] + "/" + \
+    os.environ[PACKAGING_INSTALLER_NAME] + "-" + tag + "-compat.dmg"
+macosnewfile = os.environ[OUTPUTDIR] + "/" + \
+    os.environ[PACKAGING_INSTALLER_NAME] + "-" + tag + ".dmg"
+winx64newfile = os.environ[OUTPUTDIR] + "/" + \
+    os.environ[PACKAGING_INSTALLER_NAME] + \
+    "-" + tag + "-" + "x64-winportable.zip"
+winx86newfile = os.environ[OUTPUTDIR] + "/" + \
+    os.environ[PACKAGING_INSTALLER_NAME] + \
+    "-" + tag + "-" + "x86-winportable.zip"
+
+for asset in assets:
+    if system == "linux":
+        if "AppImage" in asset.browser_download_url and os.path.isfile(linuxnewfile):
+            do_bsdiff(asset.browser_download_url, linuxnewfile)
+    elif system == "macos":
+        if "compat" in asset.browser_download_url and os.path.isfile(macoscompatnewfile):
+            do_bsdiff(asset.browser_download_url, macoscompatnewfile)
+        elif "compat" not in asset.browser_download_url and "dmg" in asset.browser_download_url and os.path.isfile(macosnewfile):
+            do_bsdiff(asset.browser_download_url, macosnewfile)
+    else:
+        if "x64-winportable" in asset.browser_download_url and os.path.isfile(winx64newfile):
+            do_bsdiff(asset.browser_download_url, winx64newfile)
+        if "x86-winportable" in asset.browser_download_url and os.path.isfile(winx86newfile):
+            do_bsdiff(asset.browser_download_url, winx86newfile)
